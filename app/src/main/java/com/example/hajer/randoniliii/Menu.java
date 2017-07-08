36252b5710fe9e06b65f9ed379c4727c084826ca
AyariHAJER <ayarihajoura@yahoo.fr>
2017-07-09 00:15:02 +0200

    rando
    rando ili_studio

---
+++ app/src/main/java/com/example/hajer/randoniliii/Menu.java
@@ -0,0 +1,8 @@
+package com.example.hajer.randoniliii;
+
+/**
+ * Created by hajer on 08/07/2017.
+ */
+
+public class Menu {
+}
---
+++ app/src/main/java/com/example/hajer/randoniliii/SignUp_Fragment.java
@@ -0,0 +1,314 @@
+package com.example.hajer.randoniliii;
+
+import android.app.ProgressDialog;
+import android.content.res.ColorStateList;
+import android.content.res.XmlResourceParser;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by hajer on 05/07/2017.
+ */
+
+public class SignUp_Fragment extends Fragment implements View.OnClickListener {
+    private static View view;
+    private static EditText fullName, emailId, mobileNumber, location,
+            password, confirmPassword;
+    private static TextView login;
+    private static Button signUpButton;
+    private static CheckBox terms_conditions;
+
+    public SignUp_Fragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.signup_layout, container, false);
+        initViews();
+        setListeners();
+        return view;
+    }
+
+    // Initialize all views
+    private void initViews() {
+        fullName = (EditText) view.findViewById(R.id.fullName);
+        emailId = (EditText) view.findViewById(R.id.userEmailId);
+        mobileNumber = (EditText) view.findViewById(R.id.mobileNumber);
+        location = (EditText) view.findViewById(R.id.location);
+        password = (EditText) view.findViewById(R.id.password);
+        confirmPassword = (EditText) view.findViewById(R.id.confirmPassword);
+        signUpButton = (Button) view.findViewById(R.id.signUpBtn);
+        login = (TextView) view.findViewById(R.id.already_user);
+        terms_conditions = (CheckBox) view.findViewById(R.id.terms_conditions);
+
+        // Setting text selector over textviews
+        XmlResourceParser xrp = getResources().getXml(R.drawable.text_selector);
+        try {
+            ColorStateList csl = ColorStateList.createFromXml(getResources(),
+                    xrp);
+
+            login.setTextColor(csl);
+            terms_conditions.setTextColor(csl);
+        } catch (Exception e) {
+        }
+    }
+
+    // Set Listeners
+    private void setListeners() {
+        signUpButton.setOnClickListener(this);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.signUpBtn:
+
+                // Call checkValidation method
+                checkValidation();
+                break;
+
+            case R.id.already_user:
+
+                // Replace login fragment
+                new MainActivity().replaceLoginFragment();
+                break;
+        }
+
+    }
+
+    // Check Validation Method
+    private void checkValidation() {
+
+        // Get all edittext texts
+        String getFullName = fullName.getText().toString();
+        String getEmailId = emailId.getText().toString();
+        String getMobileNumber = mobileNumber.getText().toString();
+        String getLocation = location.getText().toString();
+        String getPassword = password.getText().toString();
+        String getConfirmPassword = confirmPassword.getText().toString();
+
+        // Pattern match for email id
+        Pattern p = Pattern.compile(Utils.regEx);
+        Matcher m = p.matcher(getEmailId);
+
+        // Check if all strings are null or not
+        if (getFullName.equals("") || getFullName.length() == 0
+                || getEmailId.equals("") || getEmailId.length() == 0
+                || getMobileNumber.equals("") || getMobileNumber.length() == 0
+                || getLocation.equals("") || getLocation.length() == 0
+                || getPassword.equals("") || getPassword.length() == 0
+                || getConfirmPassword.equals("")
+                || getConfirmPassword.length() == 0)
+
+            new CustomToast().Show_Toast(getActivity(), view,
+                    "All fields are required.");
+
+            // Check if email id valid or not
+        else if (!m.find())
+            new CustomToast().Show_Toast(getActivity(), view,
+                    "Your Email Id is Invalid.");
+
+            // Check if both password should be equal
+        else if (!getConfirmPassword.equals(getPassword))
+            new CustomToast().Show_Toast(getActivity(), view,
+                    "Both password doesn't match.");
+
+            // Make sure user should check Terms and Conditions checkbox
+        else if (!terms_conditions.isChecked())
+            new CustomToast().Show_Toast(getActivity(), view,
+                    "Please select Terms and Conditions.");
+
+            // Else do signup or do your stuff
+        else
+            Toast.makeText(getActivity(), "Do SignUp.", Toast.LENGTH_SHORT)
+                    .show();
+
+    }
+    class PostAsync extends AsyncTask<String, String, JSONObject> {
+        JSONParser jsonParser = new JSONParser();
+
+        private ProgressDialog pDialog;
+
+        private static final String LOGIN_URL = "http://randon.ili-studios.tn/api/USERS/";
+
+        private static final String TAG_SUCCESS = "success";
+        private static final String TAG_MESSAGE = "message";
+
+
+        @Override
+        protected void onPreExecute() {
+           // pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Attempting login...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... args) {
+
+            try {
+
+                HashMap<String, String> params = new HashMap<>();
+                params.put("Full_Name", args[0]);
+                params.put("Email", args[1]);
+                params.put("Mobile_Number", args[2]);
+                params.put("Location", args[3]);
+                params.put("Password", args[4]);
+
+
+
+                Log.d("request", "starting");
+
+                JSONObject json = jsonParser.makeHttpRequest(
+                        LOGIN_URL, "POST", params);
+
+                if (json != null) {
+                    Log.d("JSON result", json.toString());
+
+                    return json;
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(JSONObject json) {
+
+            int success = 0;
+            String message = "";
+
+            if (pDialog != null && pDialog.isShowing()) {
+                pDialog.dismiss();
+            }
+
+            if (json != null) {
+               // Toast.makeText(MainActivity.this, json.toString(),
+                      //  Toast.LENGTH_LONG).show();
+
+                try {
+                    success = json.getInt(TAG_SUCCESS);
+                    message = json.getString(TAG_MESSAGE);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (success == 1) {
+                Log.d("Success!", message);
+            }else{
+                Log.d("Failure", message);
+            }
+        }
+
+    }
+
+
+    class GetAsync extends AsyncTask<String, String, JSONObject> {
+
+        JSONParser jsonParser = new JSONParser();
+
+        private ProgressDialog pDialog;
+
+        private static final String LOGIN_URL = "http://randon.ili-studios.tn/api/USERS/";
+
+        private static final String TAG_SUCCESS = "success";
+        private static final String TAG_MESSAGE = "message";
+
+        @Override
+        protected void onPreExecute() {
+           // pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Attempting login...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... args) {
+
+            try {
+
+                HashMap<String, String> params = new HashMap<>();
+                params.put("Full_Name", args[0]);
+                params.put("Email", args[1]);
+                params.put("Mobile_Number", args[2]);
+                params.put("Location", args[3]);
+                params.put("Password", args[4]);
+
+                Log.d("request", "starting");
+
+                JSONObject json = jsonParser.makeHttpRequest(
+                        LOGIN_URL, "GET", params);
+
+                if (json != null) {
+                    Log.d("JSON result", json.toString());
+
+                    return json;
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(JSONObject json) {
+
+            int success = 0;
+            String message = "";
+
+            if (pDialog != null && pDialog.isShowing()) {
+                pDialog.dismiss();
+            }
+
+            if (json != null) {
+             //  Toast.makeText(MainActivity.this, json.toString(),
+                   //   Toast.LENGTH_LONG).show();
+
+                try {
+                    success = json.getInt(TAG_SUCCESS);
+                    message = json.getString(TAG_MESSAGE);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (success == 1) {
+                Log.d("Success!", message);
+            }else{
+                Log.d("Failure", message);
+            }
+        }
+
+
+    }
+}
